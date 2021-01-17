96da27b5e19879a7f7af05d4e42bdd308f408748
SANGCO <nsg1213@gmail.com>
2021-01-17 21:52:33 +0900

    Initial commit

---
+++ src/main/java/dev/sangco/layoutproduct/LayoutProductApplication.java
@@ -0,0 +1,13 @@
+package dev.sangco.layoutproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LayoutProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LayoutProductApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/sangco/layoutproduct/LayoutProductApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sangco.layoutproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LayoutProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
